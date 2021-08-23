a19ef4a8da18aa9ea0da0753fa709f8a40676848
Lucky Okoedion <luckyokoedion2000@gmail.com>
2021-08-23 01:28:15 +0100

    cors fix

--- Startup.cs
+++ Startup.cs
@@ -38,6 +38,8 @@ namespace IsabiTextAnalysisApi
                 options.AddDefaultPolicy(builder =>
                 {
                     builder.AllowAnyOrigin();
+                    builder.AllowAnyMethod();
+                    builder.AllowAnyHeader();
                 });
             });
 
